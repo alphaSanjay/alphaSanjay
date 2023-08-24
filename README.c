4b32351269c65cc081c594ec93efe900cbb18246
SANJAY P <83605663+alphaSanjay@users.noreply.github.com>
2023-08-24 20:30:30 +0530

    Update README.c

--- README.c
+++ README.c
@@ -1,10 +1,1 @@
-🔥Hi i'm Sanjay_P
-🔥I'm doing graduation in Computer Science Engineering 
-🔥I'm from India
 
-
-
-<!---
-alphaSanjay/alphaSanjay is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
